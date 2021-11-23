39d5647d62a747884068b5e6085639f152d2cf43
Will Lovell <willard.lovell@icloud.com>
2021-11-22 23:19:54 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+const currentUser = 'Will Lovell';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const shortGreeting = `Welcome, ${firstInitial}!`;
+const firstInitial = currentUser[-10];
+const restOfName = currentUser.slice[0];
+
+
+
